002c3ad86679e4bc6bb3d1e5808812989c64ca6a
梁书聪 <8948760+liang-shucong@user.noreply.gitee.com>
2021-08-24 18:30:08 +0800

    deploy

---
+++ precache-manifest.59c5fdb9b3dd13932e8d1980df23aa72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ebb1218912e1ff2652",
+    "url": "/rembill-web/css/app.6bfcefe4.css"
+  },
+  {
+    "revision": "59a7d07d3fe3cf73eb61ba5da88418bd",
+    "url": "/rembill-web/index.html"
+  },
+  {
+    "revision": "a2ebb1218912e1ff2652",
+    "url": "/rembill-web/js/app.71591612.js"
+  },
+  {
+    "revision": "fd4b2a5014d6fc227878",
+    "url": "/rembill-web/js/chunk-vendors.9adb3a5e.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/rembill-web/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/rembill-web/robots.txt"
+  }
+]);
